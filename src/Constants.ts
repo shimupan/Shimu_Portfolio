26653375a2f1c14f67a45775373baf4603e0ef6b
shimupan <shimupan@gmail.com>
2024-03-01 18:11:09 -0500

    finished header

---
+++ src/Constants.ts
@@ -0,0 +1,18 @@
+export let HeaderItems = [
+    {
+        name: 'Home',
+        link: '/'
+    },
+    {
+        name: 'About',
+        link: '/about'
+    },
+    {
+        name: 'Projects',
+        link: '/projects'
+    },
+    {
+        name: 'Contact',
+        link: '/contact'
+    }
+];
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Hero } from '@/components/main/Hero';
+export { default as FloatingHeader } from '@/components/ui/FloatingHeader'
